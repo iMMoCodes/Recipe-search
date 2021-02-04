3a0370eef8f07868c12f9803275644fd507800b1
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-02-04 03:04:29 +0200

    added Recipe component and mapped through the data

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,12 +1,25 @@
+import React, {useEffect,useState} from "react";
+import Recipe from "./Recipe";
 import './App.css';
 require("dotenv").config();
 
 const App = () => {
 
-  const APP_KEY = process.env.FOOD_SEARCH_APP_KEY;
   const APP_ID = process.env.FOOD_SEARCH_APP_ID;
+  const APP_KEY = process.env.FOOD_SEARCH_APP_KEY;
   
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes,setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
 
   return (
     <div className="App">
@@ -14,6 +27,13 @@ const App = () => {
         <input className="search-bar" type="text" />
         <button className="search-btn" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 }
